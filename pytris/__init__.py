537a99e6a3f1bfc3bf43877e0f358885ce216c61
Hayk Khachatryan <kh.ha.hayk@gmail.com>
2019-08-18 02:24:02 +0400

    Quite a few
    - Added some constants at top
    - map:
        - added map with 1 for a block in that pos and 0 for none
        - mapCheck():
            - checks if a block can go to new position
        - mapAdd():
            - adds block to posMap
        - mapDel():
            - removes block from posMap
    - class Block:
        - changed to intake list of rectangles (rects) instead of u, w, h
        - draws from rects instead of graphics file
        - calculates width and height from rects
        - incorporated posMap, mapCheck(), mapAdd(), and mapDel()
        - does mapCheck before dropping

--- pytris/__init__.py
+++ pytris/__init__.py
@@ -6,15 +6,23 @@ __author__ = "Hayk Khachatryan"
 __version__ = '0.1.0'
 __license__ = "MIT"
 
+
+###################
+#    constants    #
+###################
+
+windowHeight = 120
+windowWidth = 80
+
+
 #################
 #    classes    #
 #################
 
-
 class Block:
     """The blocks we know and love."""
 
-    def __init__(self, u, w, h):
+    def __init__(self, r):
         """
             Initialises a block at (random x location, top of window)
 
@@ -22,61 +30,71 @@ class Block:
                 x (int):        randomly assigned x position of block in window
                                 (in multiples of 4)
                 y (int):        y position of block in window
-                vy (int):       frame gap between 4 pixel drops
-                u (int):        x location of start of block in image bank
+                vy (int):       frame interval between 4 pixel drops
+                r (list):       list of rectangles representing block [(x, y), (w, h), col] values
+                    x (int):    x coord of starting point of rectangle
+                    w (int):    x coord of ending point of rectangle  
+                    y (int):    y coord of starting point of rectangle
+                    h (int):    y coord of ending point of rectangle
+                    col (int):  color of block
                 width (int):    width of block
                 height (int):   height of block
                 falling (Bool): whether or not block is still falling
         """
+        self.rects = r
 
-        self.x = randrange((pyxel.width - w)/4) * 4
+        setWidthHeight(self)
+
+        self.x = randrange((pyxel.width - self.width)/4) * 4
         self.y = 0
         self.vy = 32
-        self.u = u
-        self.width = w
-        self.height = h
         self.falling = True
 
+        # Add block to posMap
+        mapAdd(self, posMap)
+    
     def update(self):
         """Updates block
 
-            if not at bottom of screen:
+            if not at bottom of screen OR clashing with other block:
                 falls
             else
                 not falling
         """
         
-        if ((self.y + self.height) < pyxel.height):
+        if ((self.y + self.height) < pyxel.height) and not mapCheck(self, posMap, 0, 1):
 
             # self.vy: frame gap between drops
             if (pyxel.frame_count % self.vy) == 0:
+                mapDel(self, posMap)
                 self.y = (self.y + 4)
+                mapAdd(self, posMap)
 
             if pyxel.btnp(pyxel.KEY_LEFT, 10, 1):
+                mapDel(self, posMap)
                 self.x = max(0, self.x - 4)
+                mapAdd(self, posMap)
             
             if pyxel.btnp(pyxel.KEY_RIGHT, 10, 1):
+                mapDel(self, posMap)
                 self.x = min(self.x + 4, pyxel.width - self.width)
+                mapAdd(self, posMap)
 
             if pyxel.btnp(pyxel.KEY_DOWN):
                 self.vy = 1
 
         else:
             self.falling = False
+    
+    def draw(self):
+        """Draws blocks rectangle by rectangle (from self.rects)"""
+
+        for (x, y), (w, h), col in self.rects:
+            pyxel.rect(x + self.x, y + self.y, w + self.x, h + self.y, col)
 
 
 class App:
     """Main app"""
-
-    blockData = [     # List of [u, w, h] (see Block.__doc__) for the 7 blocks
-        [0,  16, 4],  # I
-        [16, 12, 8],  # J
-        [28, 12, 8],  # L
-        [40,  8, 8],  # O
-        [48, 12, 8],  # S
-        [60, 12, 8],  # T
-        [72, 12, 8]   # Z
-    ]
    
     def __init__(self):
         """
@@ -86,14 +104,10 @@ class App:
             - runs update & draw
         """
 
-        # init 160 by 120 space
-        pyxel.init(120, 80)
-
-        # load graphics
-        pyxel.load("blocks.pyxel")
+        pyxel.init(windowWidth, windowHeight)
 
         # init a random block
-        self.blocks = [Block(*App.blockData[randrange(7)])]
+        self.blocks = [Block(blockDatann[randrange(7)])]
 
         pyxel.run(self.update, self.draw)
 
@@ -101,7 +115,7 @@ class App:
 
         # generates a new block if last block has stopped falling
         if not self.blocks[-1].falling:
-            self.blocks.append(Block(*App.blockData[randrange(7)]))
+            self.blocks.append(Block(blockDatann[randrange(7)]))
             self.blocks[-1].falling = True
 
         # update all blocks
@@ -113,22 +127,20 @@ class App:
             pyxel.quit()
 
     def draw(self):
-        
+
         # clear screen w/ black
         pyxel.cls(0)
-        for (x, y), (w, h), col in blockDatann[6]:
-            pyxel.rect(y, x, h, w, col)
-
+        
+        # draw all blocks
         for block in self.blocks:
-            pyxel.blt(block.x, block.y, 0, block.u, 0, block.width, block.height, 0)
+            block.draw()
 
 
-#            rect(x, y, w, h, col)
 ################
 #    blocks    #
 ################
 
-blockDatann = [
+blockDatann = [ # (x, y), (w, h), col
     [  # I
         [(0, 0), (15, 3), 12]
     ],
@@ -157,13 +169,58 @@ blockDatann = [
     ]
 ]
 
-# I
-# J
-# L
-# O
-# S
-# T
-# Z
+
+#############
+#    map    #
+#############
+
+# generates 2D array consisting of 0s
+posMap = [[0 for _ in range(windowWidth // 4)] for _ in range(windowHeight // 4)]
+
+
+def mapCheck(block, posMap, changeX, changeY):
+    """Checks if moving block to new position (+changeX, +changeY) will clash with existing block"""
+    for (x, y), (w, h), col in block.rects:
+        for xx in range(x, w, 4):
+            for yy in range(y, h, 4):
+                if not posMap[changeY + (yy + block.y)//4][changeX + (xx + block.x)//4]:
+                    return False
+    return True
+
+
+def mapAdd(block, posMap):
+    """Adds block to posMap"""
+    for (x, y), (w, h), col in block.rects:
+        for xx in range(x, w, 4):
+            for yy in range(y, h, 4):
+                posMap[(yy + block.y)//4][(xx + block.x)//4] = 1
+
+
+def mapDel(block, posMap):
+    """Removes block from posMap"""
+    for (x, y), (w, h), col in block.rects:
+        for xx in range(x, w, 4):
+            for yy in range(y, h, 4):
+                posMap[(yy + block.y)//4][(xx + block.x)//4] = 0
+
+
+##############
+#    misc    #
+##############
+
+def setWidthHeight(block):
+    """Set's width and height of a Block() object"""
+    block.height = 0
+    block.width = 0
+
+    for (x, y), (w, h), col in block.rects:
+        block.width = max(w, block.width)
+        block.height = max(h, block.height)
+
+    block.width += 1
+    block.height += 1
+
+
 ##################
 #    run baby    #
 ##################
